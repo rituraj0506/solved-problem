3f0da847653872a6b51b4e3d95a8a6eebf38b09c
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-05-26 21:28:30 +0530

    Time: 27 ms (18.69%), Space: 16.8 MB (13.23%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int dp[10001];
-    int f(int i,vector<int>a)
+    // same as house robber
+    int f(int i,vector<int>&a,vector<int>&dp)
     {
-        if(i<0) return 0;
-        if(i==0) return a[i];
+        if(i>=a.size()) return 0;
         if(dp[i]!=-1) return dp[i];
-        int take=a[i]+f(i-2,a);
-        int nott=0+f(i-1,a);
+        int take=f(i+2,a,dp)+a[i];
+        int nott=f(i+1,a,dp)+0;
         return dp[i]=max(take,nott);
     }
     int deleteAndEarn(vector<int>& nums) {
-       int n=nums.size();
-       vector<int> arr(10001);// indexing 0 to 10^4
-       memset(dp,-1,sizeof(dp));
-        for (int i=0;i<nums.size();i++){
-            arr[nums[i]]+=nums[i];
+       vector<int>a(10001,0);// indexing 0 to 10^4; 
+        vector<int>dp(10001,-1);
+        for(auto it:nums)
+        {
+            a[it]+=it;
         }
-        return  f(*max_element(nums.begin(),nums.end()),arr);
+       return f(0,a,dp);
     }
 };
