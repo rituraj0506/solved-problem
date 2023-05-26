4d5ce7c8edb9a8214830f22742d672410820b084
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-05-26 22:49:16 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (44.61%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool f(int i,vector<int>& piles,vector<int>&dp)
+    {
+        if(i<0) return false;
+        if(i==0) return piles[i];
+        if(dp[i]!=-1) return dp[i];
+        int take=f(i-1,piles,dp)+piles[i];
+        int nott=f(i-1,piles,dp)+piles[i];
+        return dp[i]=max(take,nott);
+    }
+    bool stoneGame(vector<int>& piles) {
+        int n=piles.size();
+        vector<int>dp(n,-1);
+        return max(f(n-1,piles,dp),f(0,piles,dp));
+    }
+};
