10a1457f68db2b89033398cc0230063a3b541589
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-06-11 19:20:32 +0530

    Time: 204 ms (5.41%), Space: 28.5 MB (19.06%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-      int f(int ind,int ind1,string&text1,string&text2,vector<vector<int>>&dp)
+    int f(int i,int j,string&s,string &t,vector<vector<int>>&dp)
     {
-        if(ind<0||ind1<0) return 0;
-        if(dp[ind][ind1]!=-1) return dp[ind][ind1];
-        if(text1[ind]==text2[ind1])
-        return dp[ind][ind1]=1+f(ind-1,ind1-1,text1,text2,dp);
-        return  dp[ind][ind1]=0+max(f(ind-1,ind1,text1,text2,dp),f(ind,ind1-1,text1,text2,dp));
-            
+        if(i<0||j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take=0,nott=0;
+        nott=f(i-1,j-1,s,t,dp);
+        if(s[i]==t[j])
+        take=1+f(i-1,j-1,s,t,dp);
+        else
+        take=max(f(i-1,j,s,t,dp),f(i,j-1,s,t,dp));
+        return dp[i][j]=max(take,nott);
     }
-    
-    int longestCommonSubsequence(string text1, string text2) {
-        int m=text1.size();
-        int n=text2.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-       return f(m-1,n-1,text1,text2,dp);
-    }
-    
-    int longestPalindromeSubseq(string s) {
-      string t=s;
-       reverse(t.begin(),t.end());
-        return longestCommonSubsequence(s,t);
+    int fun(string &s,string&t,vector<vector<int>>&dp)
+    {
+        int n=s.size();
+        int n1=t.size();
+        return f(n-1,n1-1,s,t,dp);
     }
     int minInsertions(string s) {
-        return s.size()-longestPalindromeSubseq(s);
+       int n=s.size();
+       string t=s;
+       reverse(t.begin(),t.end());
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int ans=fun(s,t,dp);
+        return n-ans;
     }
 };
