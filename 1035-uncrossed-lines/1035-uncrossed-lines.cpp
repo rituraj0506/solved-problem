53bf5e55f6c74d5e048c5b5ebef8e4368425bc4e
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-06-11 18:52:48 +0530

    Time: 47 ms (10.83%), Space: 12.9 MB (60.32%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,19 +1,21 @@
 class Solution {
-public:// same as longest common subsequence
-    int f(int ind1,int ind2,vector<int>& nums1, vector<int>& nums2,vector<vector<int>>&dp)
+public:
+    int f(int i,int j,vector<int>& nums1,vector<int>& nums2,vector<vector<int>>&dp)
     {
-        if(ind1<0||ind2<0)
-            return 0;
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        if(nums1[ind1]==nums2[ind2])
-            return dp[ind1][ind2]=1+f(ind1-1,ind2-1,nums1,nums2,dp);
+        if(i<0||j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int nott=0,take=0;
+        nott=f(i-1,j-1,nums1,nums2,dp);
+        if(nums1[i]==nums2[j])
+        take=1+f(i-1,j-1,nums1,nums2,dp);
         else
-            return dp[ind1][ind2]=0+max(f(ind1-1,ind2,nums1,nums2,dp),f(ind1,ind2-1,nums1,nums2,dp));
+            take=max(f(i-1,j,nums1,nums2,dp),f(i,j-1,nums1,nums2,dp));
+        return dp[i][j]=max(nott,take);
     }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-         int m=nums1.size();
-         int n=nums2.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,nums1,nums2,dp);
+        int n=nums1.size();
+        int n1=nums2.size();
+        vector<vector<int>>dp(n,vector<int>(n1,-1));
+       return f(n-1,n1-1,nums1,nums2,dp);
     }
 };
