7ab793c4591d1dd083c9c3def77b78155e800a0f
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-07-05 21:41:16 +0530

    Time: 0 ms (100.00%), Space: 6 MB (76.40%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
+    int f(int n,vector<int>&dp)
+    {
+     if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+    int ans=0;
+     for(int i=1;i<=n;i++)
+      ans+= f(i-1,dp)*f(n-i,dp);
+        return dp[n]=ans;
+    }
     int numTrees(int n) {
-       if(n<=1) return 1;
-        int ans=0;
-           for(int i=1;i<=n;i++)
-            ans+= numTrees(i-1)*numTrees(n-i);
-        return ans;
+      vector<int>dp(n+1,-1);
+        return f(n,dp);
     }
 };
