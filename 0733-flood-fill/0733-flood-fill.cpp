f6398f8998ce65efd045633e825f442b405ae7cf
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-08-08 21:16:17 +0530

    Time: 8 ms (70.33%), Space: 14.2 MB (26.31%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    void dfs(int r,int c,int incol,vector<vector<int>>& img,vector<vector<int>>&ans,int dr[],int dc[],int col)
-    {
-        int n=img.size();
-        int m=img[0].size();
-        ans[r][c]=col;
-        for(int i=0;i<4;i++)
-        {
-            int nr=r+dr[i];
-            int nc=c+dc[i];
- if(nr>=0&& nr<n && nc>=0 &&nc<m&&img[nr][nc]==incol && ans[nr][nc]!=col)
-     dfs(nr,nc,incol,img,ans,dr,dc,col);
-        }
-        
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int col) {
-      int incol=img[sr][sc];
-        vector<vector<int>>ans=img;
-        int dr[]={-1,0,+1,0};
-        int dc[]={0,+1,0,-1};
-        dfs(sr,sc,incol,img,ans,dr,dc,col);
-        return ans;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newcolor) {
+        int n=image.size();
+        int m=image[0].size();
+    vector<vector<int>>vis(image);
+     queue<pair<int,int>>q;
+     q.push({sr,sc});
+     vis[sr][sc]=newcolor;
+     int dr[]={-1,0,+1,0};
+     int dc[]={0,+1,0,-1};
+     int incol=image[sr][sc];
+     while(!q.empty())
+     {
+         int r=q.front().first;
+         int c=q.front().second;
+         q.pop();
+         for(int i=0;i<4;i++)
+         {
+             int nr=r+dr[i];
+             int nc=c+dc[i];
+             if(nr>=0&&nr<n&&nc>=0&&nc<m&&image[nr][nc]==incol && vis[nr][nc]!=newcolor)
+             {
+                 q.push({nr,nc});
+                 vis[nr][nc]=newcolor;
+             }
+         }
+     }
+      return vis;
     }
 };
