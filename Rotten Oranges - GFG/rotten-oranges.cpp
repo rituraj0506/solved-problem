c5657c12b227875c5f46dfa034a74bffd3974c41
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-08-10 22:22:54 +0530

    Added solution - LeetHub

---
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find minimum time required to rot all oranges. 
+    int orangesRotting(vector<vector<int>>& grid) {
+        vector<vector<int>>ans(grid);
+        int n=grid.size();
+        int m=grid[0].size();
+        int maxi=0;
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(ans[i][j]==2)
+                {
+                    q.push({{i,j},0});
+                }
+            }
+        }
+        int dr[]={-1,0,+1,0};
+        int dc[]={0,+1,0,-1};
+        while(!q.empty())
+        {
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            q.pop();
+            maxi=max(maxi,t);
+            for(int i=0;i<4;i++)
+            {
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+if(nr>=0&&nr<n && nc>=0 && nc<m && ans[nr][nc]==1 && ans[nr][nc]!=2){
+    ans[nr][nc]=2;
+    q.push({{nr,nc},t+1});
+}
+            }
+            
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(ans[i][j]==1)
+                return -1;
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
