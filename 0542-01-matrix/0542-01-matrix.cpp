da4b5c263fc6151f444df242629f409e3c084987
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-08-10 23:16:36 +0530

    Time: 63 ms (93.31%), Space: 34.4 MB (24.78%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
+      int n=mat.size(),m=mat[0].size();
       vector<vector<int>>vis(n,vector<int>(m,0));
       vector<vector<int>>dist(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++)
         {
-         for(int j=0;j<m;j++)
-         {
-             if(mat[i][j]==0)
-             {
-                 q.push({{i,j},0});
-                 vis[i][j]=1;
-             }
-         }
+            for(int j=0;j<m;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                }
+                else
+                    vis[i][j]=0;
+            }
         }
+        // you can go in adjacent dir mw=eans four dir
         int dr[]={-1,0,+1,0};
         int dc[]={0,+1,0,-1};
-        
         while(!q.empty())
         {
             int r=q.front().first.first;
@@ -31,10 +32,11 @@ public:
             {
                 int nr=r+dr[i];
                 int nc=c+dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc]){
-                    q.push({{nr,nc},stp+1});
-                   vis[nr][nc]=1;
-                }
+        if(nr>=0 && nr<n && nc>=0 && nc<m  && !vis[nr][nc])
+        {
+            q.push({{nr,nc},stp+1});
+            vis[nr][nc]=1;
+        }
             }
         }
         return dist;
