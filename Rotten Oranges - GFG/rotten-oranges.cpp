dd8cda166d82f04efd96f978d22b6e18d3d52282
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-08-12 21:56:53 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -8,50 +8,55 @@ class Solution
     public:
     //Function to find minimum time required to rot all oranges. 
     int orangesRotting(vector<vector<int>>& grid) {
-        vector<vector<int>>ans(grid);
         int n=grid.size();
         int m=grid[0].size();
-        int maxi=0;
-        queue<pair<pair<int,int>,int>>q;
-        for(int i=0;i<n;i++)
+    vector<vector<int>>vis(n,vector<int>(m,0));
+    vector<vector<int>>ans(n,vector<int>(m,0));
+    int maxi=0;
+    queue<pair<pair<int,int>,int>>q;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
         {
-            for(int j=0;j<m;j++)
+            if(grid[i][j]==2)
             {
-                if(ans[i][j]==2)
-                {
-                    q.push({{i,j},0});
-                }
+               q.push({{i,j},0});
+               vis[i][j]=2;
+            }
+            else{
+                vis[i][j]=0;
             }
         }
-        int dr[]={-1,0,+1,0};
-        int dc[]={0,+1,0,-1};
-        while(!q.empty())
-        {
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int t=q.front().second;
-            q.pop();
-            maxi=max(maxi,t);
-            for(int i=0;i<4;i++)
-            {
-                int nr=r+dr[i];
-                int nc=c+dc[i];
-if(nr>=0&&nr<n && nc>=0 && nc<m && ans[nr][nc]==1){
-    ans[nr][nc]=2;
-    q.push({{nr,nc},t+1});
+    }
+      	int dr[]={-1,0,+1,0};
+	   	int dc[]={0,+1,0,-1};
+    while(!q.empty())
+    {
+        int r=q.front().first.first;
+        int c=q.front().first.second;
+        int dis=q.front().second;
+         q.pop();
+          maxi=max(maxi,dis);
+          for(int i=0;i<4;i++)
+          {
+              int nr=r+dr[i];
+              int nc=c+dc[i];
+if(nr>=0&&nr<n && nc>=0 && nc<m&&grid[nr][nc]==1 && !vis[nr][nc])
+{
+    q.push({{nr,nc},dis+1});
+    vis[nr][nc]=2;
 }
-            }
-            
-        }
-        for(int i=0;i<n;i++)
+          }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
         {
-            for(int j=0;j<m;j++)
-            {
-                if(ans[i][j]==1)
-                return -1;
-            }
+            if(grid[i][j]==1&&!vis[i][j])
+            return -1;
         }
-        return maxi;
+    }
+    return maxi;
     }
 };
 
