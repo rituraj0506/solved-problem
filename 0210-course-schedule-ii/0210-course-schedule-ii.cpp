c13ad3edb116bb7ec9147a5c9f4d43d903024290
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-08-18 10:40:01 +0530

    Time: 20 ms (69.72%), Space: 13.8 MB (54.03%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(n);
-        vector<int>indeg(n,0);
-        vector<int>topo;
-        for(auto i:prerequisites)
+        vector<int>ans;
+         vector<int>adj[n];
+        for(auto it:prerequisites)
         {
-            adj[i[1]].push_back(i[0]);
+         adj[it[1]].push_back(it[0]);
         }
+        vector<int>indeg(n,0);
+        queue<int>q;
+        
         for(int i=0;i<n;i++)
         {
             for(auto it:adj[i])
-            {
                 indeg[it]++;
-            }
         }
-         queue<int>q;
-        for(int i=0;i<n;i++)
+        
+         for(int i=0;i<n;i++)
         {
-            if(indeg[i]==0)
-                q.push(i);
+               if(indeg[i]==0)
+                 q.push(i);
         }
+        
+        int c=0;
         while(!q.empty())
         {
             int node=q.front();
-            topo.push_back(node);
             q.pop();
+            ans.push_back(node);
+            
             for(auto it:adj[node])
             {
                 indeg[it]--;
                 if(indeg[it]==0)
                     q.push(it);
-            }    
+            }
         }
-        if(topo.size()==n) return topo;
-            return {};
-            
+        if(ans.size()==n)
+       return ans;
+        return { };
     }
 };
