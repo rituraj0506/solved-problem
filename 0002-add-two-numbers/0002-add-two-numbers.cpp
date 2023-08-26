905804c7304fef862866e8bcb0e67418da7957a7
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-08-26 19:01:44 +0530

    Time: 20 ms (88.75%), Space: 71.4 MB (31.22%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,27 +11,30 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int carry=0,sum=0;
-        ListNode*dummy=new ListNode();
+      ListNode*dummy=new ListNode();
         ListNode*temp=dummy;
-        while(l1!=NULL || l2!=NULL ||carry!=0)
+        int sum=0,carry=0;
+        while(l1!=NULL||l2!=NULL||carry!=0)
         {
             sum=carry;
-         if(l1!=NULL)
-         {
-            sum+=l1->val;
-             l1=l1->next;
-         }
-        if(l2!=NULL)
-         {
-            sum+=l2->val;
-             l2=l2->next;
-         }
+            if(l1!=NULL)
+            {
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            
+             if(l2!=NULL)
+            {
+                sum+=l2->val;
+                l2=l2->next;
+            }
             carry=sum/10;
-            ListNode*temp1=new ListNode(sum%10);
-            temp->next=temp1;
+            ListNode*newnode=new ListNode(sum%10);
+            temp->next=newnode;
             temp=temp->next;
         }
+        // temp = []
+        // dummy=[] lekin temp me dummy hai (means one single empty node) to dummy ka next to temp k first hoga
         return dummy->next;
     }
 };
