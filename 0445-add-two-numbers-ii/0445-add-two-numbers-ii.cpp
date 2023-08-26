7425bb90b71bde700c49c948e377390aabfb3d6e
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-08-26 19:28:24 +0530

    Time: 27 ms (93.34%), Space: 73.7 MB (14.42%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -12,36 +12,35 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         stack<int>s1,s2;
-        while(l1)
+        while(l1!=NULL)
         {
             s1.push(l1->val);
             l1=l1->next;
         }
-         while(l2)
+         while(l2!=NULL)
         {
             s2.push(l2->val);
-            l2=l2->next;
+             l2=l2->next;
         }
         
         ListNode*prev=NULL;
         int carry=0;
-        while(!s1.empty() || !s2.empty() || carry)
+        while(!s1.empty() ||!s2.empty() ||carry!=0)
         {
-           int sum=carry;
+            int sum=carry;
             if(!s1.empty())
             {
-                sum+=s1.top();
+               sum+=s1.top();
                 s1.pop();
             }
-            
-              if(!s2.empty())
+             if(!s2.empty())
             {
-                sum+=s2.top();
+               sum+=s2.top();
                 s2.pop();
             }
             carry=sum/10;
-           ListNode*newnode=new ListNode(sum%10);
-            newnode->next=prev;
+            ListNode*newnode=new ListNode(sum%10);
+             newnode->next=prev;
             prev=newnode;
         }
         return prev;
