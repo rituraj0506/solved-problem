a4e4105426422158664edcae35891f6102381466
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-09-01 19:58:14 +0530

    Time: 2 ms (69.86%), Space: 11.1 MB (19.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int bs(int l,int h,vector<int>& nums,int tar)
+    int bs(int l,int h,vector<int>& nums, int target)
     {
-        while(l<=h){
-        int mid=l+(h-l)/2;
-        if(nums[mid]==tar)
-            return mid;
-       else if(nums[mid]<tar)
-            l=mid+1;
-        else 
-            h=mid-1;
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+                l=mid+1;
+            else
+                h=mid-1;
         }
         return -1;
     }
@@ -17,26 +18,30 @@ public:
     {
         int n=nums.size();
         int l=0,h=n-1;
-        if(nums[l]<nums[h]) /*in the case arr is not rotated*/
+         if(nums[l]<nums[h]) /*in the case arr is not rotated*/
             return 0;
-        while(l<=h){
-        int mid=l+(h-l)/2;
-        int prev=(mid+n-1)%n;
-        int next=(mid+1)%n;
-        if(nums[mid]<=nums[prev]&& nums[mid]<=nums[next])
-            return mid;
-        if(nums[0]<=nums[mid])
-            l=mid+1;
-       else if(nums[mid]<=nums[h])
-            h=mid-1;
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            int prev=(mid+n-1)%n;
+            int next=(mid+1)%n;
+            if(nums[mid]<=nums[prev]&&nums[mid]<=nums[next])
+                return mid;
+            else if(nums[0]<=nums[mid])
+                l=mid+1;
+            else if(nums[mid]<=nums[h])
+            {
+                h=mid-1;
+            }
         }
         return -1;
     }
     int search(vector<int>& nums, int target) {
         int n=nums.size();
-      int indx=f(nums); 
-        int a=bs(0,indx-1,nums,target);
-        int b=bs(indx,n-1,nums,target);
+        int ind=f(nums);
+        cout<<ind;
+        int a=bs(0,ind-1,nums,target);
+        int b=bs(ind,n-1,nums,target);
         if(a==-1)
             return b;
         else
