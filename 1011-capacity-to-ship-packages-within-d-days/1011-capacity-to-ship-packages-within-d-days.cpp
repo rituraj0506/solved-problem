931dcf4cb40c9bc12e141a41cf9f41259f263ee6
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-09-02 18:05:42 +0530

    Time: 54 ms (78.84%), Space: 31.1 MB (28.39%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
-    bool isvalid(vector<int>&a,int day,int mid)
-   {
-       int sum=0;
+    bool isvalid(vector<int>&a,int mid,int k)
+    {
         int n=a.size();
-        int d=1;
+        int sum=0;
+        int cap=1;
         for(int i=0;i<n;i++)
         {
-            sum+=a[i];
-            if(sum>mid)
-            {
-                d++;
-                sum=a[i];
-            }
-             if(d>day)
-                 return false;
+         sum+=a[i];
+         if(sum>mid)
+         {
+             cap++;
+             sum=a[i];
+         }
+          if(cap>k)
+         return false;
         }
         return true;
-   }
+    }
+  
     int shipWithinDays(vector<int>& weights, int days) {
         int st=*max_element(weights.begin(),weights.end());
         int last=0;
-        int res=0;
+        int res=-1;
         int n=weights.size();
         for(int i=0;i<n;i++)
         {
-           last=last+=weights[i];
+            last+=weights[i];
         }
         int end=last;
         while(st<=end)
         {
             int mid=st+(end-st)/2;
-            if(isvalid(weights,days,mid))
+            if(isvalid(weights,mid,days))
             {
                 res=mid;
                 end=mid-1;
             }
             else
-                st=mid+1;
+            st=mid+1;
         }
-        return  res;
+        return res;
+       
     }
 };
