0b0bc6e952bbb93e59e10e88890afda5dd0f2b20
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-09-06 20:52:05 +0530

    Time: 0 ms (100.00%), Space: 18.6 MB (33.27%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int f(int ind,vector<int>& coins,int amt,vector<vector<int>>&dp)
+    int f(int ind,vector<int>& coins,int amount,vector<vector<int>>&dp)
     {
         if(ind==0)
         {
-            if(amt%coins[ind]==0)
+            if(amount%coins[ind]==0)
                 return 1;
             else
                 return 0;
         }
-        if(dp[ind][amt]!=-1) return dp[ind][amt];
-        int nott=f(ind-1,coins,amt,dp);
+        if(dp[ind][amount]!=-1) return dp[ind][amount];
+        int nott=f(ind-1,coins,amount,dp);
         int take=0;
-        if(coins[ind]<=amt)
-        take=f(ind,coins,amt-coins[ind],dp);
-        return dp[ind][amt]=take+nott;
+        if(coins[ind]<=amount)
+            take=f(ind,coins,amount-coins[ind],dp);
+        return dp[ind][amount]=take+nott;
     }
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return f(n-1,coins,amount,dp);
+      int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+      return f(n-1,coins,amount,dp); 
     }
 };
