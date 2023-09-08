ff7f6b2bf3482fab1a173f0e1ec00879f3851ee7
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-09-08 14:49:13 +0530

    Time: 851 ms (21.67%), Space: 292.2 MB (17.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int f(int ind,vector<int>& nums,int prev,vector<vector<int>>&dp)
+    int f(int prev,int ind,int n,vector<int>& nums, vector<vector<int>>&dp)
     {
-        if(ind==nums.size()) return 0;// if you go to out of index what len you take 0
-        int take=0,nott=0;
+        if(ind>=n)
+        {
+            return 0;
+        }
         if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-        nott=0+f(ind+1,nums,prev,dp);
-        if(prev==-1||nums[prev]<nums[ind])
-        take=1+f(ind+1,nums,ind,dp); //taking this guy and prev guy is your curr index
-        return dp[ind][prev+1]= max(take,nott);
-        
-          
+        int nott=0+f(prev,ind+1,n,nums,dp);
+        int take=-1e9;
+        if(prev==-1||nums[ind]>nums[prev])
+           take= 1+f(ind,ind+1,n,nums,dp);
+        return dp[ind][prev+1]=max(take,nott);
     }
+        
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-      vector<vector<int>>dp(n,vector<int>(n+1,-1));// how to store prev = -1 so shift 
-        return f(0,nums,-1,dp);
+        int prev=-1;
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+       return f(prev,0,n,nums,dp);
+        
     }
 };
