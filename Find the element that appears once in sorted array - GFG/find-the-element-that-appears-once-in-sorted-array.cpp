103a5be6c5452b26b1ad0c18b71f1b6e16e8b22f
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-10-19 11:47:23 +0530

    Added solution - LeetHub

--- Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.cpp
+++ Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.cpp
@@ -11,27 +11,23 @@ class Solution
   public:
     int findOnce(int arr[], int n)
     {
-    //     int l=0,h=n-1;
-    //     if(arr[0]!=arr[1])
-    //      return arr[0];
-    //  else if(arr[n-1]!=arr[n-2]) return arr[n-1];
-    //     while(l<=h)
-    //     {
-    //       int mid=l+(h-l)/2;
-    //       if(arr[mid]!=arr[mid+1]&&arr[mid]!=arr[mid-1])
-    //       return arr[mid];
-    //      else if((mid%2==0||arr[mid]==arr[mid-1])||(mid%2==1 || arr[mid]==arr[mid+1]))
-    //       l=mid+1;
-    //       else
-    //       h=mid-1;
-    //     }
-    //     return -1;
-        int ans=arr[0];
-        for(int i=1;i<n;i++)
+        int l=0,h=n-1;
+        if(n==1) return arr[0];
+        if(arr[0]!=arr[1])
+         return arr[0];
+     else if(arr[h]!=arr[h-1]) return arr[h];
+        while(l<=h)
         {
-           ans^=arr[i];
+          int mid=l+(h-l)/2;
+          if(arr[mid]!=arr[mid+1]&&arr[mid]!=arr[mid-1])
+          return arr[mid];
+         else if((mid%2==0 && arr[mid]==arr[mid+1])||(mid%2==1 && arr[mid]==arr[mid-1]))
+          l=mid+1;
+          else
+          h=mid-1;
         }
-        return ans;
+        return -1;
+        
     }
 };
 
