8735a039b47cbe4b0cd96d030b1b5163ea3c2f1b
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-10-30 10:19:50 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,51 +6,51 @@ using namespace std;
 class Solution {
   public:
     // Function to find the number of islands.
-    void bfs(int r,int c,vector<vector<char>>& grid, vector<vector<int>>&vis)
+    void bfs(int r,int c,vector<vector<char>>&grid,vector<vector<int>>&vis)
     {
         int n=grid.size();
         int m=grid[0].size();
-        vis[r][c]=1;
         queue<pair<int,int>>q;
         q.push({r,c});
+        vis[r][c]=1;
         while(!q.empty())
         {
-             r=q.front().first;
-             c=q.front().second;
+            auto it=q.front();
+              r=it.first;
+              c=it.second;
             q.pop();
-            for(int dr=-1;dr<=1;dr++)
+           for(int dr=-1;dr<=1;dr++)
+           {
+            for(int dc=-1;dc<=1;dc++)
             {
-                for(int dc=-1;dc<=1;dc++)
-                {
-                  int nr=r+dr;
-                  int nc=c+dc;
-                  
- if(nr>=0&&nr<n&&nc>=0 &&nc<m&& grid[nr][nc]=='1'&&!vis[nr][nc])
- {
-            vis[nr][nc]=1;
-            bfs(nr,nc,grid,vis);
- }
-                }
+                 int nr=r+dr;
+                 int nc=c+dc;
+if(nr>=0&&nr<n && nc>=0 && nc<m && grid[nr][nc]=='1'&&!vis[nr][nc])
+{
+    q.push({nr,nc});
+    vis[nr][nc]='1';
+}
             }
+           }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        int c=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]=='1' && !vis[i][j])
-                {
-                    c++;
-                    bfs(i,j,grid,vis);
-                }
-            }
-        }
-        return c;
+    int n=grid.size();
+    int m=grid[0].size();
+    vector<vector<int>>vis(n,vector<int>(m,0));
+    int c=0;
+     for(int i=0;i<n;i++)
+     {
+     for(int j=0;j<m;j++)
+     {
+       if(grid[i][j]=='1'&&!vis[i][j])
+       {
+           c++;
+           bfs(i,j,grid,vis);
+       }
+     }
+     }
+     return c;
     }
 };
 
