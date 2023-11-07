ac62709ac5c72dc17e2df8c33e9d0713a479b64d
RITURAJ KUMAR <106878457+rituraj0506@users.noreply.github.com>
2023-11-07 10:21:11 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -10,19 +10,21 @@ class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    int solve(int i,int j,string&s1,string &s2,vector<vector<int>>&dp)
+    int f(int ind1,int ind2,string &s1,string &s2,vector<vector<int>>&dp)
     {
-        if(i<0||j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j])
-         return dp[i][j]=1+solve(i-1,j-1,s1,s2,dp);
-         else
-         return dp[i][j]=0+max(solve(i-1,j,s1,s2,dp),solve(i,j-1,s1,s2,dp));
+        if(ind1<0||ind2<0) return 0;
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(s1[ind1]==s2[ind2]) 
+        return dp[ind1][ind2]=1+f(ind1-1,ind2-1,s1,s2,dp);
+        else
+        return dp[ind1][ind2]=
+        0+max(f(ind1-1,ind2,s1,s2,dp),f(ind1,ind2-1,s1,s2,dp));
     }
+    
     int lcs(int n, int m, string s1, string s2)
     {
     vector<vector<int>>dp(n,vector<int>(m,-1));
-     return solve(n-1,m-1,s1,s2,dp);
+   return f(n-1,m-1,s1,s2,dp);
     }
 };
 
